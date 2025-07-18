9368a6663ff5d40698260ebb47ed5ddde0d80db9
Mücahit A Akpınar <69982121+ByPlatin@users.noreply.github.com>
2025-07-18 17:55:31 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace pngTranslucent
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(69, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Select File :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(160, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(160, 85);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Select";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(330, 85);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Convert";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(685, 179);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Transslucent";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+using System.Drawing.Imaging;
+namespace pngTranslucent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fullPath = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Bir dosya seçin",
+                Filter = "Resim Dosyalarý (*.png)|*.png",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                fullPath = selectedFilePath;
+                label2.Text = selectedFilePath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fullPath.Count() < 1)
+            {
+                MessageBox.Show("Please select file!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string inputPath = fullPath;  // Ýþlenecek PNG dosyasýnýn yolu
+            string outputPath = fullPath.Replace(".png", "") + "_t.png"; // Çýktý PNG dosyasýnýn yolu
+
+            // Görseli yükle
+            using (Bitmap bitmap = new Bitmap(inputPath))
+            {
+                try
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    for (int x = 0; x < bitmap.Width; x++)
+                    {
+                        for (int y = 0; y < bitmap.Height; y++)
+                        {
+                            Color pixelColor = bitmap.GetPixel(x, y);
+
+                            int gray = pixelColor.A;
+                            Color newColor = Color.FromArgb(255, gray, gray, gray);
+                            bitmap.SetPixel(x, y, newColor);
+                        }
+                    }
+                    bitmap.Save(outputPath, ImageFormat.Png);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                finally
+                {
+                    fullPath = "";
+                    label2.Text = "Please select file!";
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "Please select file!";
+        }
+    }
+}
